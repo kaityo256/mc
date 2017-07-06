8f8717a125fc282da942edfa94e67e09a30083ce
H. Watanabe <kaityo@users.sourceforge.jp>
2017-07-06 20:59:32 +0900

    refactoring

--- ising_sw/main.cpp
+++ ising_sw/main.cpp
@@ -1,33 +1,19 @@
 //----------------------------------------------------------------------
 #include <iostream>
 #include <random>
-#include <math.h>
+#include <vector>
+#include <algorithm>
+#include <numeric>
 //----------------------------------------------------------------------
 const int L = 64;       // System Size
-const int T_LOOP = 100; // Thermalization loop
-const int S_LOOP = 100; // Number of average
+const int N = L*L;      // Number of spins
+const int T_LOOP = 1000; // Thermalization loop
+const int S_LOOP = 1000; // Number of average
 const int O_LOOP = 10;  // Number of observation at a temperature
 const int ND = 20;      // Number of temperatures to be observed
-int spin[L][L];
-int cluster[L * L];
-int flip[L * L];
-//----------------------------------------------------------------------
+std::vector<int> spin(N), cluster(N), flip(N);
 std::mt19937 mt(1);
 std::uniform_real_distribution<double> ud(0.0, 1.0);
-//----------------------------------------------------------------------
-double
-myrand(void) {
-  return ud(mt);
-}
-//----------------------------------------------------------------------
-void
-init(void) {
-  for (int i = 0; i < L; i++) {
-    for (int j = 0; j < L; j++) {
-      spin[i][j] = 1;
-    }
-  }
-}
 //----------------------------------------------------------------------
 int
 get_cluster_number(int index) {
@@ -39,7 +25,11 @@ get_cluster_number(int index) {
 }
 //----------------------------------------------------------------------
 void
-connect_bond(int ix1, int iy1, int ix2, int iy2) {
+connect_bond(int ix1, int iy1, int ix2, int iy2, const double p) {
+  if (ix2 == L)ix2 = 0;
+  if (iy2 == L)iy2 = 0;
+  if (spin[ix1 + iy1 * L] * spin[ix2 + iy2 * L] < 0)return;
+  if (ud(mt) >= p)return;
   const int i1 = ix1 + iy1 * L;
   const int i2 = ix2 + iy2 * L;
   const int c1 = get_cluster_number(i1);
@@ -53,65 +43,40 @@ connect_bond(int ix1, int iy1, int ix2, int iy2) {
 //----------------------------------------------------------------------
 void
 cluster_flip(double beta) {
-  const double p = 1.0 - exp(-2.0 * beta);
-  for (int i = 0; i < L * L; i++) {
+  for (int i = 0; i < N; i++) {
     cluster[i] = i;
-    flip[i] = static_cast<int>(myrand() * 2.0) * 2 - 1;
+    flip[i] = static_cast<int>(ud(mt) * 2.0) * 2 - 1;
   }
-
-  for (int ix = 0; ix < L - 1; ix++) {
-    for (int iy = 0; iy < L; iy++) {
-      if (spin[ix][iy] * spin[ix + 1][iy] > 0 && myrand() < p) {
-        connect_bond(ix, iy, ix + 1, iy);
-      }
-    }
-  }
-
-  for (int iy = 0; iy < L; iy++) {
-    if (spin[L - 1][iy] * spin[0][iy] > 0 && myrand() < p) {
-      connect_bond(L - 1, iy, 0, iy);
-    }
-  }
-
-  for (int ix = 0; ix < L; ix++) {
-    for (int iy = 0; iy < L - 1; iy++) {
-      if (spin[ix][iy] * spin[ix][iy + 1] > 0 && myrand() < p) {
-        connect_bond(ix, iy, ix, iy + 1);
-      }
-    }
-  }
-
-  for (int ix = 0; ix < L; ix++) {
-    if (spin[ix][L - 1] * spin[ix][0] > 0 && myrand() < p) {
-      connect_bond(ix, L - 1, ix, 0);
-    }
-  }
-
+  const double p = 1.0 - exp(-2.0 * beta);
   for (int ix = 0; ix < L; ix++) {
     for (int iy = 0; iy < L; iy++) {
-      const int c = get_cluster_number(ix + iy * L);
-      spin[ix][iy] = spin[ix][iy] * flip[c];
+      connect_bond(ix, iy, ix + 1, iy, p );
+      connect_bond(ix, iy, ix, iy + 1, p );
     }
+  }
+  for (int i=0;i < N;i++){
+    const int c = get_cluster_number(i);
+    spin[i] = flip[c];
   }
 }
 //----------------------------------------------------------------------
 void
 single_flip(double beta) {
-  for (int ix = 0; ix < L; ix++) {
-    for (int iy = 0; iy < L; iy++) {
-      double de = 0;
-      if (ix != 0)de += spin[ix - 1][iy];
-      else de += spin[L - 1][iy];
-      if (iy != 0)de += spin[ix][iy - 1];
-      else de += spin[ix][L - 1];
-      if (ix != L - 1)de += spin[ix + 1][iy];
-      else de += spin[0][iy];
-      if (iy != L - 1)de += spin[ix][iy + 1];
-      else de += spin[ix][0];
-      de = de * 2.0 * spin[ix][iy];
-      if (de < 0 || exp(-beta * de) > myrand()) {
-        spin[ix][iy] = - spin[ix][iy];
-      }
+  for(int i=0;i<N;i++){
+    int ix = i % L;
+    int iy = i /L;
+    double de = 0.0;
+    if (ix != 0)de += spin[(ix - 1) + iy * L];
+    else de += spin[(L - 1) + iy * L];
+    if (iy != 0)de += spin[ix + (iy - 1)*L];
+    else de += spin[ix + (L - 1)*L];
+    if (ix != L - 1)de += spin[(ix + 1) + iy*L];
+    else de += spin[0 + iy * L];
+    if (iy != L - 1)de += spin[ix + (iy + 1)*L];
+    else de += spin[ix + 0 * L];
+    de = de * 2.0 * spin[ix + iy * L];
+    if (de < 0 || exp(-beta * de) > ud(mt)) {
+      spin[i] = - spin[i];
     }
   }
 }
@@ -128,27 +93,9 @@ mc_onestep(double beta) {
 // Observation
 //----------------------------------------------------------------------
 double
-magnetization(void) {
-  double m = 0;
-  for (int ix = 0; ix < L; ix++) {
-    for (int iy = 0; iy < L; iy++) {
-      m += spin[ix][iy];
-    }
-  }
-  m = m / (double)(L * L);
-  if (m < 0) m = -m;
-  return m;
-}
-//----------------------------------------------------------------------
-double
 magnetization_square(void) {
-  double m = 0;
-  for (int ix = 0; ix < L; ix++) {
-    for (int iy = 0; iy < L; iy++) {
-      m += spin[ix][iy];
-    }
-  }
-  m /= static_cast<double>(L * L);
+  double m = std::accumulate(spin.begin(),spin.end(),0.0);
+  m /= static_cast<double>(N);
   return m * m;
 }
 //----------------------------------------------------------------------
@@ -157,14 +104,14 @@ energy(void) {
   double e = 0.0;
   for (int ix = 0; ix < L; ix++) {
     for (int iy = 0; iy < L; iy++) {
-      const int s = spin[ix][iy];
-      if (ix != L - 1) e -= s * spin[ix + 1][iy];
-      else        e -= s * spin[0][iy];
-      if (iy != L - 1) e -= s * spin[ix][iy + 1];
-      else        e -= s * spin[ix][0];
+      const int s = spin[ix + iy* L];
+      if (ix != L - 1) e -= s * spin[ix + 1 + iy*L];
+      else        e -= s * spin[0 + iy* L];
+      if (iy != L - 1) e -= s * spin[ix +(iy + 1)*L];
+      else        e -= s * spin[ix + 0 * L];
     }
   }
-  e /= static_cast<double>(L * L);
+  e /= static_cast<double>(N);
   return e;
 }
 //----------------------------------------------------------------------
@@ -174,8 +121,8 @@ domc(void) {
   double be = 0.6; //End inverse temperature (The lowest temperature)
   for (int i = 0; i < ND; i++) {
     double beta = bs + (be - bs) * (double)i / (double)ND;
-    fprintf(stderr, "beta = %f\n", beta);
-    init();
+    std::cerr << "beta = " << beta << std::endl;
+    std::fill(spin.begin(), spin.end(), 1);
     for (int j = 0; j < T_LOOP; j++) {
       mc_onestep(beta);
     }
@@ -193,8 +140,8 @@ domc(void) {
       m2 /= static_cast<double>(S_LOOP);
       e_sum /= static_cast<double>(S_LOOP);
       e2_sum /= static_cast<double>(S_LOOP);
-      const double c = (e2_sum - e_sum * e_sum) * beta * beta * L * L;
-      printf("%f %f %f\n", beta, m2, c);
+      const double c = (e2_sum - e_sum * e_sum) * beta * beta * N;
+      std::cout << beta << " " << m2 << " " << c << std::endl;
     }
   }
 }
@@ -202,12 +149,11 @@ domc(void) {
 int
 main(void) {
 #ifdef CLUSTER
-  fprintf(stderr, "Swendsen-Wang\n");
-  printf("# beta, Magnetization^2, Specific Heat\n");
+  std::cerr << "Swendsen-Wang" << std::endl;
 #else
-  fprintf(stderr, "Single Flip\n");
-  printf("# beta, Magnetization^2, Specific Heat\n");
+  std::cerr << "Single Flip" << std::endl;
 #endif
+  std::cout << "# beta, Magnetization^2, Specific Heat" << std::endl;
   domc();
 }
 //----------------------------------------------------------------------
