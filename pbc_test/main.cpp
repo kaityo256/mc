fe8090e46d5ec3144d4abb3d58ae2913aeca7f73
H. Watanabe <kaityo@users.sourceforge.jp>
2019-09-24 09:53:54 +0900

    use chrono

--- pbc_test/main.cpp
+++ pbc_test/main.cpp
@@ -1,21 +1,20 @@
 #include <algorithm>
+#include <array>
+#include <chrono>
 #include <cmath>
 #include <iostream>
 #include <numeric>
 #include <random>
 #include <vector>
-#include <array>
 
-const int L = 64;        // System Size
-const int N = L * L;     // Number of spins
-const int T_LOOP = 1000; // Thermalization loop
-const int O_LOOP = 1000;   // Number of observations
-const int ND = 20;       // Number of temperatures to be observed
+const int L = 64;    // System Size
+const int N = L * L; // Number of spins
 double e_table[5];
+
 std::vector<int> spin(N), cluster(N), flip(N);
 std::vector<std::array<int, 4>> f_neighbor(N);
 
-std::mt19937 mt(2);
+std::mt19937 mt(1);
 std::uniform_real_distribution<double> ud(0.0, 1.0);
 
 class svariable {
@@ -35,10 +34,16 @@ public:
   double error() {
     double ave = average();
     double ave2 = std::accumulate(data2.begin(), data2.end(), 0.0) / data2.size();
-    double v = (ave2 - ave*ave)/(data.size()-1);
+    double v = (ave2 - ave * ave) / (data.size() - 1);
     return sqrt(v);
   }
 };
+
+int pos2index(int ix, int iy) {
+  ix = (ix + L) % L;
+  iy = (iy + L) % L;
+  return ix + iy * L;
+}
 
 void single_flip(double beta) {
   for (int i = 0; i < N; i++) {
@@ -48,7 +53,7 @@ void single_flip(double beta) {
     ns += spin[f_neighbor[i][2]];
     ns += spin[f_neighbor[i][3]];
     ns *= spin[i];
-    if (e_table[ns/2+2] > ud(mt)){
+    if (e_table[ns / 2 + 2] > ud(mt)) {
       spin[i] = -spin[i];
     }
   }
@@ -60,35 +65,19 @@ double magnetization_square(void) {
   return m * m;
 }
 
-double energy(void) {
-  double e = 0.0;
-  for (int ix = 0; ix < L; ix++) {
-    for (int iy = 0; iy < L; iy++) {
-      const int s = spin[ix + iy * L];
-      if (ix != L - 1)
-        e -= s * spin[ix + 1 + iy * L];
-      else
-        e -= s * spin[0 + iy * L];
-      if (iy != L - 1)
-        e -= s * spin[ix + (iy + 1) * L];
-      else
-        e -= s * spin[ix + 0 * L];
-    }
-  }
-  e /= static_cast<double>(N);
-  return e;
-}
-
-void make_table(double beta){
-  for(int i=0;i<5;i++){
-    int ns = (i-2)*4;
-    e_table[i] = exp(-ns*beta);
+void make_table(double beta) {
+  for (int i = 0; i < 5; i++) {
+    int ns = (i - 2) * 4;
+    e_table[i] = exp(-ns * beta);
   }
 }
 
 void domc(void) {
-  double bs = 0.3; //Start inverse temperature  (The highest temperature)
-  double be = 0.6; //End inverse temperature (The lowest temperature)
+  const int T_LOOP = 1000; // Thermalization loop
+  const int O_LOOP = 1000; // Number of observations
+  const int ND = 20;       // Number of temperatures to be observed
+  double bs = 0.3;         //Start inverse temperature  (The highest temperature)
+  double be = 0.6;         //End inverse temperature (The lowest temperature)
   for (int i = 0; i < ND; i++) {
     double beta = bs + (be - bs) * (double)i / (double)ND;
     make_table(beta);
@@ -103,36 +92,48 @@ void domc(void) {
       single_flip(beta);
       double m2 = magnetization_square();
       sm2.add(m2);
-      sm4.add(m2*m2);
+      sm4.add(m2 * m2);
     }
     double am2 = sm2.average();
     double am4 = sm4.average();
-    double U = am4/(am2*am2);
+    double U = am4 / (am2 * am2);
 
-    std::cout << beta << " " << sm2.average() << " " <<sm2.error();
+    std::cout << beta << " " << sm2.average() << " " << sm2.error();
     std::cout << " " << U << std::endl;
   }
 }
 
-int pos2index(int ix, int iy){
-  ix = (ix + L) % L;
-  iy = (iy + L) % L;
-  return ix + iy * L;
-}
-
-void init_neighbors(){
-  for(int iy = 0; iy < L;iy++){
-    for(int ix = 0; ix < L;ix++){
+void init_neighbors() {
+  for (int iy = 0; iy < L; iy++) {
+    for (int ix = 0; ix < L; ix++) {
       int i = pos2index(ix, iy);
-      f_neighbor[i][0] = pos2index(ix+1,iy);
-      f_neighbor[i][1] = pos2index(ix-1,iy);
-      f_neighbor[i][2] = pos2index(ix,iy+1);
-      f_neighbor[i][3] = pos2index(ix,iy-1);
+      f_neighbor[i][0] = pos2index(ix + 1, iy);
+      f_neighbor[i][1] = pos2index(ix - 1, iy);
+      f_neighbor[i][2] = pos2index(ix, iy + 1);
+      f_neighbor[i][3] = pos2index(ix, iy - 1);
     }
   }
 }
 
+/*
+  Check the update speed at the critical point beta_c = log(1+sqrt(2))/2
+ */
+void speed_test(void) {
+  double beta_c = 0.5 * log(1.0 + sqrt(2.0));
+  make_table(beta_c);
+  std::fill(spin.begin(), spin.end(), 1);
+  const int LOOP = 10000;
+  const auto s = std::chrono::system_clock::now();
+  for (int i = 0; i < LOOP; i++) {
+    single_flip(0.001);
+  }
+  const auto e = std::chrono::system_clock::now();
+  const auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(e - s).count();
+  std::cout << beta_c << " " << magnetization_square() << " " << elapsed << "[msec]" << std::endl;
+}
+
 int main(void) {
   init_neighbors();
-  domc();
+  //domc();
+  speed_test();
 }
